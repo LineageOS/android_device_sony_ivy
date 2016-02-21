fe80280e129aa4cb807a6545084e49ef3bdd99df
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    ivy: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c

--- init_ivy/init_ivy.cpp
+++ init_ivy/init_ivy.cpp
@@ -22,6 +22,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <sys/types.h>
 
 #include "vendor_init.h"
@@ -29,6 +31,16 @@
 #include "log.h"
 #include "util.h"
 
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 static void import_kernel_nv(const std::string& key,
         const std::string& value, bool for_emulator __attribute__((unused))) 
