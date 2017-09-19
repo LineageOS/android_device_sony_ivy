5d54672e242a628d9b8661a135fb182d85d3fb4c
Olivier Karasangabo <olivier@lineageos.org>
2017-09-19 12:11:27 +0200

    ivy: Use vendor/lineage instead of vendor/cm
    Change-Id: I42568068e3ba3ac101562b8e788385b2282b087a

--- lineage.mk
+++ lineage.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, device/sony/ivy/full_ivy.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_ivy
